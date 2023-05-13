74e6c2125a1f0df47fb64adae18e0931ef69cd0a
KylashSri <kylashsri@gmail.com>
2023-05-13 15:44:34 +1000

    reorganised code in while loop to make initial user input (for setting weight & height) outside of while-loop, also added help command

--- src/app.py
+++ src/app.py
@@ -6,14 +6,27 @@ user_height = input("What is your height (in cm)? ")
 daily_rec_cal = get_calorie_recommendation(user_weight, user_height)
 current_cal_intake = 0
 
+print("recommended daily calorie intake is " + daily_rec_cal + " calories")
+print("your current calorie intake today is " + current_cal_intake + " calories")
+meal_name = input("what have you eaten today? ")
+meal_calories = input("how many calories did it have? ") 
+if(meal_calories != meal_calories): # error handle for NaN
+    print("meal_calories is must be a number")
+    break
+print("your recommended calorie intake is " + get_calorie_recommendation(user_weight, user_height))
+
 while:
-    print("recommended daily calorie intake is " + daily_rec_cal + " calories")
-    print("your current calorie intake today is " + current_cal_intake + " calories")
-    meal_name = input("what have you eaten today? ")
-    meal_calories = input("how many calories did it have? ") 
-    if(meal_calories != meal_calories): # error handle for NaN
-        print("meal_calories is must be a number")
-        break
+    print("type 'help' for command list")
+    command = input()
+    if(command == 'help')
+        print("type 'help' to get the list of commands")
+        print("type '' to ")
+        print("type '' to ")
+        print("type '' to ")
+        print("type '' to ")
+        print("type '' to ")
+        print("type '' to ")
+        print("type 'quit' to quit")
     
 
 def get_calorie_recommendation(weight, height):
@@ -26,3 +39,5 @@ def set_user_weight():
 def set_user_height():
     user_height = input("What is your height (in cm)? ")
     return
+
+
