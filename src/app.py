3b68da3e671647d6403ef726989d0c67357634ea
KylashSri <kylashsri@gmail.com>
2023-05-13 16:47:03 +1000

    fixed compile errors, took away unnecesary checking for if height or weight was not number

--- src/app.py
+++ src/app.py
@@ -1,4 +1,3 @@
-
 def get_calorie_recommendation(weight, height):
     return 14*weight + 5*height
 
@@ -13,31 +12,25 @@ def set_user_height():
 
 meal_list = []
 current_cal_intake = 0
-
+print("welcome to Calorie Tracker")
 print("this program allows you to track how many calories you've had and compare it against a recommendation")
 
-user_weight = input("What is your weight (in kg)? ")
-while user_weight != user_weight:
-    print(user_weight + " is not a valid input, please enter a number")
-    user_weight = input("What is your weight (in kg)? ")
-
-user_height = input("What is your height (in cm)? ")
-while user_height != user_height:
-    print(user_height + " is not a valid input, please enter a number")
-    user_height = input("What is your height (in cm)? ")
+user_weight = float(input("What is your weight (in kg)? "))
+user_height = float(input("What is your height (in cm)? "))
 
 daily_rec_cal = get_calorie_recommendation(user_weight, user_height)
-print("your recommended daily calorie intake is " + get_calorie_recommendation(user_weight, user_height))
+print("your recommended daily calorie intake is " + str(get_calorie_recommendation(user_weight, user_height)))
 
-print("your current calorie intake today is " + current_cal_intake + " calories")
+print("your current calorie intake today is " + str(current_cal_intake) + " calories")
 # meal_name = input("what have you eaten today? ")
 # meal_calories = input("how many calories did it have? ") 
 # if(meal_calories != meal_calories): # error handle for NaN
 #     print("meal_calories is must be a number")
 
+print("type 'help' for command list")
+
 while True:
-    print("type 'help' for command list")
-    command = input()
+    command = input("> ")
     if(command == 'help'):
         print("type 'help' to get the list of commands")
         print("type 'info' to show current weight, height and current calorie intake")
@@ -47,16 +40,13 @@ while True:
         print("type 'rec' to view recommended calorie intake")
         print("type 'ml' to view meal list (meals taken that effect calorie count)")
         print("type 'reset' to reset meal list")
-        print("type 'hardreset' to reset meal list and weight")
+        print("type 'hardreset' to reset meal list and user info")
         print("type 'quit' to quit")
     elif(command == 'info'):
         # loop through meal_list to find current calorie intake
         print("info")
     elif(command == 'meal'):
         # get user input to add a meal in tuple form to 'meal_list' variable
-        print("info")
-    elif(command == 'meal'):
-        # add meal
         print("info")
     elif(command == 'sw'):
         # set weight
@@ -73,13 +63,14 @@ while True:
     elif(command == 'reset'):
         # reset meal list
         print("info")
-    elif(command == 'reset'):
-        # reset meal list
+    elif(command == 'hardreset'):
+        # reset meal list and get prompted
         print("info")
-    elif(command == 'quit'):
+    elif(command == 'quit' or command == "q"):
         break
     else:
-        print(command + " is not a command!")
+        print("'" + command + "' is not a command!")
+        print("type 'help' for command list")
     
 
 
